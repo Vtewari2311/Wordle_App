e7976ceb43c4b0dc21d4098a7ac6e8ef63e9578a
Vtewari2311 <74680679+Vtewari2311@users.noreply.github.com>
2022-09-12 10:36:22 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/wordleapp/MainActivity.kt
+++ app/src/main/java/com/example/wordleapp/MainActivity.kt
@@ -4,9 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val upgradeButton = findViewById<Button>(R.id.button)
+        val word = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            if(counter == 1) {
+                FirstGuess.text = enterWord.text
+            }
+            if(counter == 2) {
+                SecondGuess.text = enterWord.text
+            }
+            if(counter == 2) {
+                ThirdGuess.text = enterWord.text
+            }
+            if(counter == 2) {
+                word.text = wordToGuess
+                buttonRestart.visibility = View.VISIBLE
+            }
+        }
+        
     }
     /**
      * Parameters / Fields:
