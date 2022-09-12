859ee4fe60c58cca205a1af5dbcad1c2047c2897
Vedant Tewari <vtewar@uwm.edu>
2022-09-12 07:33:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wordleapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.wordleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    /**
+     * Parameters / Fields:
+     *   wordToGuess : String - the target word the user is trying to guess
+     *   guess : String - what the user entered as their guess
+     *
+     * Returns a String of 'O', '+', and 'X', where:
+     *   'O' represents the right letter in the right place
+     *   '+' represents the right letter in the wrong place
+     *   'X' represents a letter not in the target word
+     */
+    private val wordToGuess = Array<Char>(4)
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            result += if (guess[i] == wordToGuess[i]) {
+                "O"
+            } else if (guess[i] in wordToGuess) {
+                "+"
+            } else {
+                "X"
+            }
+        }
+        return result
+    }
+}
