7ba8fa3b63a4415871055a777403df568511e7b5
Vedant Tewari <vtewar@uwm.edu>
2022-09-12 23:39:23 -0500

    Changes

--- app/src/main/java/com/example/wordleapp/MainActivity.kt
+++ app/src/main/java/com/example/wordleapp/MainActivity.kt
@@ -1,73 +1,77 @@
 package com.example.wordleapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    val wordToGuess = Array<Char>(4)
-    wordToGuess = getRandomFourLetterWord()
+    private var wordToGuess : String = FourLetterWordList.getRandomFourLetterWord()
     var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
                 
-        funView.hideKeyboard() {
+        fun View.hideKeyboard() {
             val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(windowToken, 0)
         }
         
-        fun markButtonDisable(button: Button) {
-            button?.isEnabled = false
-            button.isClickable = false
-            button?.setTextColor(ContextCompat.getColor(textView.context, R.color.white))
-            button?.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.greyish))
-        }
-        
         setContentView(R.layout.activity_main)
         
-        var editText = findViewById<EditText>(R.id.editText)
-        val button = findViewById<Button>(R.id.button)
-        val buttonRestart = findViewbyId<Button>(R.id.guess)
+        var editText = findViewById<EditText>(R.id.keyboard)
+        val buttonRestart = findViewById<Button>(R.id.guess)
         val word = findViewById<TextView>(R.id.word)
-        val textView = findViewById<Button>(R.id.textView)
-        val textViewforGuess1 = findViewById<TextView>(R.id.FirstGuess)
-        val textViewforGuess2 = findViewById<TextView>(R.id.SecondGuess)
-        val textViewforGuess3 = findViewById<TextView>(R.id.ThirdGuess)
         val textViewGuessOutput1 = findViewById<TextView>(R.id.FirstGuessOutput)
         val textViewGuessOutput2 = findViewById<TextView>(R.id.SecondGuessOutput)
         val textViewGuessOutput3 = findViewById<TextView>(R.id.ThirdGuessOutput)
-        val checkViewforGuess1 = findViewById<TextView>(R.id.FirstGuessCheck)
-        val checkViewforGuess2 = findViewById<TextView>(R.id.SecondGuessCheck)
-        val checkViewforGuess3 = findViewById<TextView>(R.id.ThirdGuessCheck)
-        
+        val checkViewForGuessOutput1 = findViewById<TextView>(R.id.FirstGuessOutputCheck)
+        val checkViewForGuessOutput2 = findViewById<TextView>(R.id.SecondGuessOutputCheck)
+        val checkViewForGuessOutput3 = findViewById<TextView>(R.id.ThirdGuessOutputCheck)
+
+        fun markButtonDisable(button: Button) {
+            button.isEnabled = false
+            button.isClickable = false
+            button.setTextColor(ContextCompat.getColor(buttonRestart.context, R.color.white))
+            button.setBackgroundColor(ContextCompat.getColor(buttonRestart.context, R.color.grey))
+        }
+
         var counter = 0
         word.visibility = View.GONE
         
-        button.setOnClickListener {
+        buttonRestart.setOnClickListener {
             Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
             it.hideKeyboard()
             counter++
-            if(counter == 1) {
-                textViewGuessOutput1.text = editText.text.toString().uppercase()
-                var guess1 = checkGuess(textViewGuessOutput1.text)
-                it.hideKeyBoard()
-                checkViewforGuess1.text = guess1
-                editText.getText().clear()
+            when (counter) {
+                1 -> {
+                    textViewGuessOutput1.text = editText.text.toString().uppercase()
+                    var guess1 = checkGuess(textViewGuessOutput1.text as String)
+                    it.hideKeyboard()
+                    checkViewForGuessOutput1.text = guess1
+                    editText.text.clear()
+                }
+                2 -> {
+                    textViewGuessOutput2.text = editText.text.toString().uppercase()
+                    var guess2 = checkGuess(textViewGuessOutput2.text as String)
+                    it.hideKeyboard()
+                    checkViewForGuessOutput2.text = guess2
+                    editText.text.clear()
+                }
+                3 -> {
+                    textViewGuessOutput3.text = editText.text.toString().uppercase()
+                    var guess3 = checkGuess(textViewGuessOutput3.text as String)
+                    it.hideKeyboard()
+                    checkViewForGuessOutput3.text = guess3
+                    editText.text.clear()
+                }
             }
-            else if(counter == 2) {
-                textViewGuessOutput2.text = editText.text.toString().uppercase()
-                var guess2 = checkGuess(textViewGuessOutput2.text)
-                it.hideKeyBoard()
-                checkViewforGuess1.text = guess1
-                editText.getText().clear()
-            }
-            else if(counter == 3) {
-                textViewGuessOutput1.text = editText.text.toString().uppercase()
-                var guess3 = checkGuess(textViewGuessOutput3.text)
-                it.hideKeyBoard()
-                checkViewforGuess3.text = guess3
-                editText.getText().clear()
-            }
-            buttonRestart.visibility = markButtonDisable(buttonRestart)
+            markButtonDisable(buttonRestart)
         }
         
     }
